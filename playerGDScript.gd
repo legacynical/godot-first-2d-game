cb19da3e8aaf8abfa02b656b416c0bf89fef3e0b
Legacynical <legacynical@gmail.com>
2024-06-03 14:32:22 -0700

    Add player collision

--- playerGDScript.gd
+++ playerGDScript.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed = 400 # player move speed (pixels/sec)
 var screen_size # size of game window
@@ -6,6 +7,7 @@ var screen_size # size of game window
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,10 +35,25 @@ func _process(delta):
 	position = position.clamp(Vector2.ZERO, screen_size) # clamping to prevent player leaving screen
 
 	if velocity.x != 0: # if moving left or right
-		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.animation = "walk" # name is cap sensitive!
 		$AnimatedSprite2D.flip_v = false # ensures sprite isn't vert flipped
 		$AnimatedSprite2D.flip_h = velocity.x < 0 # sprite (facing right) flips only if moving left
 	elif velocity.y != 0: # if moving up or down without left/right movement
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0 # sprite (facing up) flips only if moving down
 		
+
+func _on_body_entered(body):
+	hide() # player disappears after being hit
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+		# disables collision after hit to prevent multiple hit signal triggers
+		# Disabling the area's collision shape can cause an error if it happens in the middle of the
+		# engine's collision processing. Using set_deferred() tells Godot to wait to disable the 
+		# shape until it's safe to do so.
+		
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false	
