efdc5dfe3f67978afa9dc183d41d45569a6343e6
Legacynical <legacynical@gmail.com>
2024-06-03 13:56:52 -0700

    Implement player movement

--- playerGDScript.gd
+++ playerGDScript.gd
@@ -10,4 +10,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO # player movement vector (direction)
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	# normalize sets diagonal length consistent to hori/vert movement to prevent faster diagonal speed
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed 
+		$AnimatedSprite2D.play() # $ is shorthand for get_node() [1/3]
+	else:
+		$AnimatedSprite2D.stop() # get_node("AnimatedSprite2D").stop() [2/3]
+	# In GDScript $ returns relative node path from current node or null if not found.
+	# Since AnimatedSprite2D is child of current node, we can use $AnimatedSprite2D [3/3]
+	
+	position += velocity * delta # updates player position
+	position = position.clamp(Vector2.ZERO, screen_size) # clamping to prevent player leaving screen
