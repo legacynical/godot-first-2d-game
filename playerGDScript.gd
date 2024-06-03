ec9dede670c6844fb04eb0a2d96b4edfdedf4d20
Legacynical <legacynical@gmail.com>
2024-06-03 13:27:46 -0700

    Add project input map for movement

--- playerGDScript.gd
+++ playerGDScript.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+@export var speed = 400 # player move speed (pixels/sec)
+var screen_size # size of game window
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
